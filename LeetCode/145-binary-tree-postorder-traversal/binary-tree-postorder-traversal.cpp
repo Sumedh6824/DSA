27f15ff63e93d0611e318fdde014e22f565f5e00
Sumedh6824 <127741895+Sumedh6824@users.noreply.github.com>
2026-09-22 22:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (93.05%) - LeetSync

--- LeetCode/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ LeetCode/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -12,15 +12,41 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> temp;
-        helper(root,temp);
-        return temp;
-    }
-private:
-    void helper(TreeNode* root, vector<int>& temp){
-        if(root == NULL)return;
-        helper(root->left,temp);
-        helper(root->right,temp);
-        temp.push_back(root->val);
+//         vector<int> temp;
+//         helper(root,temp);
+//         return temp;
+//     }
+// private:
+//     void helper(TreeNode* root, vector<int>& temp){
+//         if(root == NULL)return;
+//         helper(root->left,temp);
+//         helper(root->right,temp);
+//         temp.push_back(root->val);
+        vector<int> postorder;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while(cur != NULL || !st.empty()){
+            if(cur != NULL){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    postorder.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        postorder.push_back(temp->val);
+                    }
+                }
+                else{
+                    cur = temp;
+                }
+            }
+        }
+        return postorder;
     }
 };
